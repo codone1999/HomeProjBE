2525505ac8640d076e91e63392565486fd56a8dc
Thiti Prasopsub <auethiti@gmail.com>
2025-04-21 16:25:52 +0700

    updatebe

---
+++ src/main/java/org/example/homeprojbe/HomeprojbeApplication.java
@@ -0,0 +1,13 @@
+package org.example.homeprojbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeprojbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeprojbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/homeprojbe/HomeprojbeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.homeprojbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeprojbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
